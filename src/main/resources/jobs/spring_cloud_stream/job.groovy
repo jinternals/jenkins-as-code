1670d6325688dc3e223178c0944d2002aa47a7f9
Mradul Pandey <pandeymradul@gmail.com>
2018-08-03 18:49:43 +0530

    Updated spring cloud stream

--- src/main/resources/jobs/spring_cloud_stream/job.groovy
+++ src/main/resources/jobs/spring_cloud_stream/job.groovy
@@ -5,13 +5,13 @@ import javaposse.jobdsl.dsl.DslFactory
 DslFactory factory = this
 
 // The simplest job is the FreestyleJob
-factory.job("spring-cloud-job-build") {
+factory.job("spring-cloud-stream-build") {
     // Required by the Delivery Pipeline view
     deliveryPipelineConfiguration("Build")
     // Trigger when a push to github was made
     triggers { githubPush() }
     // SCM configuration
-    scm { github("jinternals/spring-cloud-job") }
+    scm { github("jinternals/spring-cloud-stream") }
     // Additional wrappers
     wrappers { colorizeOutput() }
     // Steps to be executed
@@ -29,24 +29,24 @@ factory.job("spring-cloud-job-build") {
         // automatic step
         // for manual use buildPipelineTrigger
         downstreamParameterized {
-            trigger("spring-cloud-job-deploy") { triggerWithNoParameters() }
+            trigger("spring-cloud-stream-deploy") { triggerWithNoParameters() }
         }
     }
 }
 
-factory.job("spring-cloud-job-deploy") {
+factory.job("spring-cloud-stream-deploy") {
     // Required by the Delivery Pipeline view
     deliveryPipelineConfiguration("Deploy")
     // SCM configuration
-    scm { github("jinternals/spring-cloud-job") }
+    scm { github("jinternals/spring-cloud-stream") }
     // Steps to be executed
     steps { shell("echo 'Deploying artifact'") }
 }
 
 
-factory.deliveryPipelineView("spring-cloud-job-pipeline") {
+factory.deliveryPipelineView("spring-cloud-stream-pipeline") {
     pipelines {
-        component("Deployment", "spring-cloud-job-build")
+        component("Deployment", "spring-cloud-stream-build")
     }
     allowPipelineStart()
     showChangeLog()
