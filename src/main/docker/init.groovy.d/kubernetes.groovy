5fea3ed3d903a35c6d73792e2703fe5851a9cd9e
Mradul <pandeymradul@gmail.com>
2019-07-15 14:06:16 +0530

    Create kubernetes.groovy

---
+++ src/main/docker/init.groovy.d/kubernetes.groovy
@@ -0,0 +1,19 @@
+import org.csanchez.jenkins.plugins.kubernetes.*
+import jenkins.model.*
+
+def instance = Jenkins.getInstance()
+
+def kubernetesCloud = new KubernetesCloud('jenkins')
+
+kubernetesCloud.setServerUrl('https://192.168.64.7:8443')
+kubernetesCloud.setNamespace('jenkins')
+kubernetesCloud.setJenkinsUrl('http://jenkins-ui.jenkins:8080')
+kubernetesCloud.setSkipTlsVerify(false)
+kubernetesCloud.setJenkinsTunnel('jenkins-discovery.jenkins:50000')
+kubernetesCloud.setConnectTimeout(5)
+kubernetesCloud.setReadTimeout(15)
+
+
+instance.clouds.replace(kubernetesCloud)
+
+instance.save()
