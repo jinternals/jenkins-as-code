4339adf66ad56243f5093a77ffd3c431f3ad7557
Mradul <pandeymradul@gmail.com>
2019-07-22 21:02:53 +0530

    Update default-user.groovy

--- src/main/docker/init.groovy.d/default-user.groovy
+++ src/main/docker/init.groovy.d/default-user.groovy
@@ -4,11 +4,14 @@ import hudson.security.GlobalMatrixAuthorizationStrategy
 
 def env = System.getenv()
 
+
+config = readYaml (file: '/etc/jenkins-secret-volume/config.yml')
+
 def jenkins = Jenkins.getInstance()
 jenkins.setSecurityRealm(new HudsonPrivateSecurityRealm(false))
 jenkins.setAuthorizationStrategy(new GlobalMatrixAuthorizationStrategy())
 
-def user = jenkins.getSecurityRealm().createAccount(env.JENKINS_USER, env.JENKINS_PASS)
+def user = jenkins.getSecurityRealm().createAccount(config.username, config.password)
 user.save()
 
-jenkins.getAuthorizationStrategy().add(Jenkins.ADMINISTER, env.JENKINS_USER)
+jenkins.getAuthorizationStrategy().add(Jenkins.ADMINISTER, config.username)
