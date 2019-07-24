9bb49964ca0fb6098717b7cde1b92f4d02abf4cd
Mradul <pandeymradul@gmail.com>
2019-07-24 22:49:27 +0530

    Update seed-job.groovy

--- src/main/docker/jobs/seed-job.groovy
+++ src/main/docker/jobs/seed-job.groovy
@@ -1,23 +1,7 @@
-pipelineJob('seed-job') {
+@Library("jenkins-demo-shared-libraries") _
 
-    def repo = 'https://github.com/jinternals/jenkins-jobs-configuration.git'
-
-    triggers {
-        scm('H/5 * * * *')
-    }
-
-    description("Seed Job")
-
-    definition {
-        cpsScm {
-            scm {
-                git {
-                    remote { url(repo) }
-                    branches('master')
-                    scriptPath('Jenkinsfile')
-                    extensions { }  // required as otherwise it may try to tag the repo, which you may not want
-                }
-            }
-        }
-    }
-}
+seedPipeline(
+        gitBranch: "master",
+        gitCredentialId: "github",
+        gitRepository: "https://github.com/jinternals/jenkins-jobs-configuration.git"
+)
